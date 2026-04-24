61d64f0a4f6e0dccd1c65102f17376d531778edb
Aridho1 <muhamadaridho859@gmail.com>
2026-04-24 10:50:33 +0700

    feat: add auth middleware

---
+++ middlewares/auth_middleware.go
@@ -0,0 +1,48 @@
+package middlewares
+
+import (
+	"net/http"
+	"strings"
+	"zunn/backend-api/config"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtKey = []byte(config.GetEnv("JTW_SECRET", "MY_VERY_SECRET_STRINGS123///"))
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func (c *gin.Context)  {
+		
+		tokenString := c.GetHeader("Authorization")
+
+		if tokenString == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Token is required",
+			})
+
+			c.Abort()
+			return
+		}
+
+		tokenString = strings.TrimPrefix(tokenString, "Bearer ")
+
+		claims := &jwt.RegisteredClaims{}
+
+		token, err := jwt.ParseWithClaims(tokenString, claims, func(t *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		})
+		
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Invalid Token",
+			})
+			c.Abort()
+			return
+		}
+
+		c.Set("Username", claims.Subject)
+
+		c.Next()
+	}
+}
