c6764abd0cb92fa7256397030feb97505653f03b
Aridho1 <muhamadaridho859@gmail.com>
2026-04-24 10:54:20 +0700

    feat: add helpers dan hash pw

---
+++ helpers/hash.go
@@ -0,0 +1,11 @@
+package helpers
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) string {
+	hashed, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	return string(hashed)
+}
