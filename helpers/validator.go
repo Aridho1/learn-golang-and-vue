4ac565d5a6cf295ed65a8be57717d43198c91c2f
Aridho1 <muhamadaridho859@gmail.com>
2026-04-28 11:02:50 +0700

    feat: add struct response for error dan success. also struct user for add, get, edit, and login

---
+++ helpers/validator.go
@@ -0,0 +1,57 @@
+package helpers
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+	"gorm.io/gorm"
+)
+
+func TranslateErrorMessage(err error) map[string]string {
+	errorsMap := make(map[string]string)
+
+	if validationErrors, ok := err.(validator.ValidationErrors); ok {
+		for _, fieldError := range validationErrors {
+			field := fieldError.Field()
+
+			switch fieldError.Tag() {
+				case "required":
+					errorsMap[field] = fmt.Sprintf("%s is required", field)
+				case "email":
+					errorsMap[field] = "Invalid email format"
+				case "unique":
+					errorsMap[field] = fmt.Sprintf("%s already exists", field)
+				case "min":
+					errorsMap[field] = fmt.Sprintf("%s must be at least %s characters", field, fieldError.Param())
+				case "max":
+					errorsMap[field] = fmt.Sprintf("%s must be at most %s characters", field, fieldError.Param())
+				case "numeric":
+					errorsMap[field] = fmt.Sprintf("%s must be number", field)
+				default:
+					errorsMap[field] = "Invalid value"
+			}
+		}
+	}
+
+	if err != nil {
+		_err := err.Error()
+		if strings.Contains(_err, "Duplicate Entry") {
+			if strings.Contains(_err, "username") {
+				errorsMap["username"] = "Username already exists"
+			} 
+			
+			if strings.Contains(_err, "email") {
+				errorsMap["email"] = "Email already exists"
+			}
+		} else if err == gorm.ErrRecordNotFound {
+			errorsMap["Error"] = "Record not found"
+		}
+	}
+
+	return errorsMap
+}
+
+func IsDuplicateEntryError(err error) bool {
+	return err != nil && strings.Contains(err.Error(), "Duplicate Entry")
+}
