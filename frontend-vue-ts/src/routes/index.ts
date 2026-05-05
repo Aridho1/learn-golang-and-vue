1eda66d56821d91f1d18609a9b265c648628de8c
Aridho1 <muhamadaridho859@gmail.com>
2026-05-05 09:44:28 +0700

    feat(fe): add view and route dashboard

--- frontend-vue-ts/src/routes/index.ts
+++ frontend-vue-ts/src/routes/index.ts
@@ -1,3 +1,5 @@
+import Cookies from "js-cookie";
+
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
 const routes: RouteRecordRaw[] = [
@@ -16,11 +18,31 @@ const routes: RouteRecordRaw[] = [
         name: "login",
         component: () => import("../views/auth/login.vue"),
     },
+    {
+        path: "/dashboard",
+        name: "dashboard",
+        component: () => import("../views/admin/dashboard/index.vue"),
+        meta: { requiresAuth: true },
+    },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
+});
+
+const getToken = () => Cookies.get("token");
+
+router.beforeEach((to, _from, next) => {
+    const token = getToken();
+
+    if (!token && to.matched.some((record) => record?.meta?.requiresAuth)) {
+        next({ name: "logim" });
+    } else if (token && (to.name == "login" || to.name == "register")) {
+        next({ name: "dashboard" });
+    } else {
+        next();
+    }
 });
 
 export default router;
